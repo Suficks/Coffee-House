777f768d183060b8d522e27b58e3036c495183ef
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-11-10 16:09:24 +0300

    feat: add Enjoy section

--- app/src/App/App.jsx
+++ app/src/App/App.jsx
@@ -1,10 +1,12 @@
 import './app.scss';
 import Header from '../Components/Header/Header'
+import Enjoy from '../Components/Enjoy/Enjoy';
 
 const App = () => {
   return (
     <div className="wrapper">
       <Header />
+      <Enjoy />
     </div>
   );
 }
---
+++ app/src/components/Enjoy/Enjoy.jsx
@@ -0,0 +1,15 @@
+import './enjoy.scss'
+
+const Enjoy = () => {
+  return (
+    <section className="enjoy">
+      <div className="text__wrap">
+        <h1 className="title"><span className="italic">Enjoy</span> premium coffee at our charming cafe</h1>
+        <h2 className="subtitle">With its inviting atmosphere and delicious coffee options, the Coffee House Resource is a popular destination for coffee lovers and those seeking a warm and inviting space to enjoy their favorite beverage.</h2>
+      </div>
+      <button className="button">Menu</button>
+    </section>
+  )
+}
+
+export default Enjoy
