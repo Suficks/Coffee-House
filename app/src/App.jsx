7710ad8d02ea02fad173360a0a469fdf78ef5ddc
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-11-10 12:23:01 +0300

    feat: add normalize

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,25 +1,10 @@
-import { useState } from 'react'
-import './App.scss';
-import Header from './Header'
+import './styles/App.scss';
+import Header from './components/Header/Header.jsx'
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
-  const handleClick = () => {
-    alert('kf kf magic')
-  }
-
   return (
     <div className="App">
-      {count > 5 && (
-        <Header count={count} isTop={true} />
-      )}
-      <button onClick={() => setCount(count + 1)}>
-        увеличить
-      </button>
-      <button onClick={() => setCount(count - 1)}>
-        уменьшить
-      </button>
+
     </div>
   );
 }
