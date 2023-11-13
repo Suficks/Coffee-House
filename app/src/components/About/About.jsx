bcd65011ec5485f3428d24a601dc91e603b9756c
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-11-13 15:05:02 +0300

    feat: add hover to photos in about section

--- app/src/components/About/About.jsx
+++ app/src/components/About/About.jsx
@@ -10,12 +10,20 @@ const About = () => {
       <h2 className="title">Resource is <span className="italic">the perfect and cozy place</span> where you can enjoy a variety of hot beverages, relax, catch up with friends, or get some work done.</h2>
       <div className="container">
         <div className="box">
-          <img src={About1} className="aboutPic" alt="aboutPic" />
-          <img src={About2} className="aboutPic" alt="aboutPic" />
+          <div className="wrap">
+            <img src={About1} className="aboutPic" alt="aboutPic" />
+          </div>
+          <div className="wrap">
+            <img src={About2} className="aboutPic small" alt="aboutPic" />
+          </div>
         </div>
         <div className="box">
-          <img src={About3} className="aboutPic" alt="aboutPic" />
-          <img src={About4} className="aboutPic" alt="aboutPic" />
+          <div className="wrap">
+            <img src={About3} className="aboutPic small" alt="aboutPic" />
+          </div>
+          <div className="wrap">
+            <img src={About4} className="aboutPic" alt="aboutPic" />
+          </div>
         </div>
       </div>
     </section>
