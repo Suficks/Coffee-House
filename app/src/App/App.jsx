b9fccf75e5e6a22c44041a21deff48cf700c413d
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-11-14 17:55:31 +0300

    feat: add pages change

--- app/src/App/App.jsx
+++ app/src/App/App.jsx
@@ -1,21 +1,35 @@
 import './app.scss';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { useRef } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import Home from '../Pages/Home';
 import Menu from '../Pages/Menu';
 
 const App = () => {
+  const location = useLocation();
+  const nodeRef = useRef();
+
   return (
     <div className="wrapper">
-      <Router basename="/">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <Header />
+      <TransitionGroup>
+        <CSSTransition
+          key={location.key}
+          classNames="fade"
+          timeout={2100}
+          nodeRef={nodeRef}
+        >
+          <div ref={nodeRef}>
+            <Routes location={location}>
+              <Route path="/" element={<Home />} />
+              <Route path="/menu" element={<Menu />} />
+            </Routes>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
+      <Footer />
     </div>
   );
 }
--- app/src/Pages/Menu/Menu.jsx
+++ app/src/Pages/Menu/Menu.jsx
@@ -1,39 +1,51 @@
 import './menu.scss';
 import { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import MenuItem from './MenuItem';
 import { coffeeData } from './Data/coffee';
 import { teaData } from './Data/tea';
 import { dessertData } from './Data/desserts';
 
+const products = {
+  coffee: coffeeData,
+  tea: teaData,
+  dessert: dessertData,
+}
+
 const Menu = () => {
-  const [typeProduct, setTypeProduct] = useState(coffeeData);
+  const [typeProduct, setTypeProduct] = useState('coffee');
 
   return (
     <main className="menu">
       <h2 className="title">Behind each of our cups hides an <span className="italic">amazing surprise</span></h2>
       <div className="filter_container">
         <button
-          className="filter coffee"
-          onClick={() => setTypeProduct(coffeeData)}
+          className={`filter coffee ${typeProduct === 'coffee' ? 'active' : null}`}
+          onClick={() => setTypeProduct('coffee')}
         >
           Coffee
         </button>
         <button
-          className="filter tea"
-          onClick={() => setTypeProduct(teaData)}
+          className={`filter tea ${typeProduct === 'tea' ? 'active' : null}`}
+          onClick={() => setTypeProduct('tea')}
         >
           Tea
         </button>
         <button
-          className="filter dessert"
-          onClick={() => setTypeProduct(dessertData)}
+          className={`filter dessert ${typeProduct === 'dessert' ? 'active' : null}`}
+          onClick={() => setTypeProduct('dessert')}
         >
           Dessert
         </button>
       </div>
-      <div className="container">
-        <MenuItem typeProduct={typeProduct} />
-      </div>
+      <CSSTransition
+        classNames="fade"
+        timeout={2100}
+      >
+        <div className="container">
+          <MenuItem typeProduct={products[typeProduct]} />
+        </div>
+      </CSSTransition>
     </main>
   )
 }
